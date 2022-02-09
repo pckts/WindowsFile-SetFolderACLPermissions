d3dd9dee4963bd08c265b9353ddf2ca51e933411
Packet <atl@pck.dk>
2022-02-09 16:13:05 +0100

    Add files via upload

---
+++ WindowsFile-SetFolderACLPermissions.ps1
@@ -0,0 +1,21 @@
+# Re-usable function for setting the same user or group permissions on multiple identically named folders
+# Can be used as-is or rewritten to fit a specific usecase.
+
+# Usage example:
+# Set-ACLPermissions WORKGROUP\packet FullControl Allow D:\Dev\Projects Code
+# This example gives the user WORKGROUP\packet the FullControl permissions on all folders called "Code" within the D:\Dev\Projects folder and all subfolders
+
+#Note: This has not been tested and may contain errors and incorrectly assumed variable types
+
+function Set-FolderACLPermissions([int]$SecurityPrincipal, [int]$PermissionType, [int]$PermissionState, [int]$SearchDir, [int]$SearchTarget) 
+{
+    $PermissionScope = New-Object System.Security.AccessControl.FileSystemAccessRule($SecurityPrincipal,$PermissionType,'ContainerInherit,ObjectInherit','None',$PermissionState)
+    $TargetFolders = Get-ChildItem -Directory -Path '\\?\'+$SearchDir -Recurse -Filter $SearchTarget | select-object FullName
+    foreach($TargetFolder in $TargetFolders)
+    {
+        $Acl = Get-ACL -Path $TargetFolder.FullName.substring(4)
+        $Acl.SetAccessRuleProtection($false,$true)
+        $Acl.AddAccessRule($PermissionScope)
+        Set-Acl $TargetFolder.FullName.substring(4) $ACL
+    }   
+}
